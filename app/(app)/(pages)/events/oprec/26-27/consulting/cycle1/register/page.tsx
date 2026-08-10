ecf0715871aec1aae0afae5cbc59f41e38183d99
Ahsan Wiryawan <ahsan.wiryawan@gmail.com>
2026-08-10 10:18:21 +0700

    feat: open CA oprec

--- app/(app)/(pages)/events/oprec/26-27/consulting/cycle1/register/page.tsx
+++ app/(app)/(pages)/events/oprec/26-27/consulting/cycle1/register/page.tsx
@@ -1,12 +1,12 @@
-// import Form from "@/components/modules/oprec/26-27/consulting/cycle1/Form";
+import Form from "@/components/modules/oprec/26-27/consulting/cycle1/Form";
 // import ComingSoon from "@/components/modules/comingsoon/ComingSoon";
-import Closed from "@/components/modules/closed/Closed";
+// import Closed from "@/components/modules/closed/Closed";
 
 export default function Page() {
   return (
     <>
-      {/* <Form /> */}
-      <Closed />
+      <Form />
+      {/* <Closed /> */}
       {/* <ComingSoon /> */}
     </>
   );
